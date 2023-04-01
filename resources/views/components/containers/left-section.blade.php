7e0ffc2b90ee7fb9573c0e7c3d44fa232de66083
DiazRodAr <diazrodrigoar@gmail.com>
2023-04-01 01:58:12 -0300

    refactory: se crean componentes container para la estructura principal. Se reestructura el modulo my post con estos componentes.

---
+++ resources/views/components/containers/left-section.blade.php
@@ -0,0 +1,6 @@
+<div class="basis-0 md:basis-1/3 xl:basis-1/4
+                    self-auto static
+                    md:self-start md:sticky md:top-6
+                    ">
+    {{$slot}}
+</div>
---
+++ resources/views/components/containers/main-section.blade.php
@@ -0,0 +1,3 @@
+<div class="basis-0 md:basis-2/3 xl:basis-2/4">
+    {{$slot}}
+</div>
