d7a05d37f559adeec42220f682c1ae9642ca7e4f
DiazRodAr <diazrodrigoar@gmail.com>
2023-04-04 22:00:47 -0300

    feature: se permite que  el usuario propietario del post pueda darle like a su propio post.

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -150,30 +150,29 @@
                 <div class="relative  overflow-hidden   rounded-b-lg bg-white text-[0.8125rem] leading-5 text-slate-900   ring-slate-700/10">
                     <div class="flex justify-evenly gap-4 border-none p-2">
                         {{-- Like button --}}
-                        @if ($user->id != Auth::user()->id)
-                            <div class="flex items-center">
+                        <div class="flex items-center">
+                            @php
+                                $userLikeToPostActive = '';
+                                if (count($post->likes) != 0) { /*likes: si esta vacio indica que el autenticado no likeo el post*/
+                                    $userLikeToPostActive = 'text-red-500';
+                                }
+                            @endphp
+                            <x-buttons.icon-button-secondary class="{{$userLikeToPostActive}} border-opacity-0 shadow-none flex" id="button-like-post" data="{{$post->id}}">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                                </svg>
+                            </x-buttons.icon-button-secondary>
+                            <p class="text-base text-gray-700 font-semibold">
                                 @php
-                                    $userLikeToPostActive = '';
-                                    if (count($post->likes) != 0) { /*likes: si esta vacio indica que el autenticado no likeo el post*/
-                                        $userLikeToPostActive = 'text-red-500';
+                                    $numberOfLikes = '';
+                                    if ($post->likes_count != 0) {
+                                        $numberOfLikes = $post->likes_count;
                                     }
                                 @endphp
-                                <x-buttons.icon-button-secondary class="{{$userLikeToPostActive}} border-opacity-0 shadow-none flex" id="button-like-post" data="{{$post->id}}">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                                    </svg>
-                                </x-buttons.icon-button-secondary>
-                                <p class="text-base text-gray-700 font-semibold">
-                                    @php
-                                        $numberOfLikes = '';
-                                        if ($post->likes_count != 0) {
-                                            $numberOfLikes = $post->likes_count;
-                                        }
-                                    @endphp
-                                    {{$numberOfLikes}}
-                                </p>
-                            </div>
-                        @endif
+                                {{$numberOfLikes}}
+                            </p>
+                        </div>
+
 
                         {{-- Show comments   --}}
                         <div class="flex items-center">
