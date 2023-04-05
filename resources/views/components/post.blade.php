598ab12b8ff012a7aae925a90d3a8443998db323
DiazRodAr <diazrodrigoar@gmail.com>
2023-04-05 15:31:09 -0300

    stylefix: se modifican los estilos del formulario de comentar post para hacerlo adaptativos en mobile

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -7,7 +7,7 @@
                  {{-- User information --}}
                  <div class="flex items-center p-4 pl-4">
                     {{-- User profile image --}}
-                    <div class="basis">
+                    <div class="basis min-w-[40px]">
                         <img class="w-8 h-8 rounded-full mx-auto object-cover"
                             src="{{$user->profile_image}}" alt=""
                         >
@@ -112,7 +112,7 @@
                 {{-- Comment --}}
                 <div class="flex items-center gap-4 p-4 py-2 pl-4">
 
-                    <div class="basis">
+                    <div class="hidden md:flex basis">
                         <img class="w-8 h-8 rounded-full mx-auto object-cover"
                             src="{{Auth::user()->profile_image}}" alt="user's  profile image"
                         >
@@ -132,11 +132,13 @@
                     <div class="basis flex">
                         <div >
                             <x-primary-button id="submit-comment-form-{{$post->id}}" type="submit"  form="form-comment-post-{{$post->id}}" >
-                                <svg id="comment-button-{{$post->id}}" class="animate-spin -ml-1 mr-2 h-4 w-4 hidden" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                <svg id="comment-button-spinner-{{$post->id}}" class="animate-spin h-5 w-5 hidden" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                     <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                                     <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                                 </svg>
-                                Comment
+                                <svg id="comment-button-{{$post->id}}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5s h-5">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+                                </svg>
                             </x-primary-button>
                         </div>
                     </div>
