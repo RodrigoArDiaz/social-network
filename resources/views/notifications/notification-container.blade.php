72c9ebe4b9ef4db06cb00b78925b99c0a57d7d2d
DiazRodAr <diazrodrigoar@gmail.com>
2023-04-17 13:29:43 -0300

    feature: se añadio boton de notificaciones

---
+++ resources/views/notifications/notification-container.blade.php
@@ -0,0 +1,18 @@
+<div>
+    <div class="px-3 pt-3">
+        <h3 class="font-medium">Notifications</h3>
+    </div>
+    {{-- Content --}}
+    <div class=" divide-y divide-slate-400/20 rounded-lg bg-transparent leading-5 text-slate-900   ring-slate-700/10">
+        <x-notifications.notification-card/>
+        <x-notifications.notification-card/>
+        <x-notifications.notification-card/>
+        <x-notifications.notification-card/>
+        <x-notifications.notification-card/>
+        <x-notifications.notification-card/>
+        <x-notifications.notification-card/>
+        <x-notifications.notification-card/>
+        <x-notifications.notification-card/>
+        <x-notifications.notification-card/>
+    </div>
+</div>
